31c82cc32a89f6972d4d26ab89fa4bee031c654d
Steve W <stephenwhalley42@gmail.com>
2018-01-07 00:14:37 +0000

    Add project files.

---
+++ AmigoForm/Models/BugReportModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AmigoForm.Models
+{
+    public class BugReportModel
+    {
+        public int ID { get; set; }
+
+        [StringLength(60,MinimumLength = 3)]
+        [Required(ErrorMessage ="Your name is required")]
+        public string CustomerName { get; set; }
+
+        [Required(ErrorMessage ="Your email is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "A description of the bug is required")]
+        public string BugDesc { get; set; }
+    
+        public string BugType { get; set; }
+    }
+}
+
---
+++ AmigoForm/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AmigoForm.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
